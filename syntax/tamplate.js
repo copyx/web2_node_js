aacb05b8fb13b381e4cdbfa33d531118b6bc00d8
Jake Kim <copyx00@gmail.com>
2018-07-09 11:49:27 +0900

    "Node.js - URL로 입력된 값 사용하기"까지 진행
    https://opentutorials.org/course/3332/21046

---
+++ syntax/tamplate.js
@@ -0,0 +1,13 @@
+const name = 'k8805';
+
+// String literal
+let letter = 'Dear ' + name + '\n\
+    \n\
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ' + name + ' Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa egoing qui officia deserunt mollit anim id est laborum. ' + name;
+console.log(letter);
+
+// Template literal
+letter = `Dear ${name}
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ${name} Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa egoing qui officia deserunt mollit anim id est laborum. ${name}`;
+console.log(letter);
