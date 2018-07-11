869c9df5676a507d9cc4267832f77839e8203436
Jake Kim <copyx00@gmail.com>
2018-07-11 11:55:51 +0900

    "JavaScript - 반복문"까지 진행
    https://opentutorials.org/course/3332/21119

--- main.js
+++ main.js
@@ -1,19 +1,74 @@
 let http = require('http');
 let url = require('url');
+let fs = require('fs');
 
 let app = http.createServer(function (request, response) {
     let _url = request.url;
     let queryData = url.parse(_url, true).query;
-    console.log(queryData);
-    if (_url === '/') {
-        _url = '/index.html';
+    let pathname = url.parse(_url, true).pathname;
+    let title = queryData.id;
+
+    if (pathname === '/') {
+        if (queryData.id === undefined) {
+            fs.readFile(`data/${queryData.id}`, 'utf8', function (err, data) {
+                let title = 'Welcome';
+                let description = 'Hello, Node.js';
+                let template = `
+                <!doctype html>
+                <html>
+                <head>
+                  <title>WEB1 - ${title}</title>
+                  <meta charset="utf-8">
+                </head>
+                <body>
+                  <h1><a href="/">WEB</a></h1>
+                  <ol>
+                    <li><a href="/?id=HTML">HTML</a></li>
+                    <li><a href="/?id=CSS"">CSS</a></li>
+                    <li><a href="/?id=JavaScript"">JavaScript</a></li>
+                  </ol>
+                  <h2>${title}</h2>
+                  <p>
+                  ${description}
+                  </p>
+                </body>
+                </html>
+                `;
+                response.writeHead(200);
+                response.end(template);
+            });
+        } else {
+                fs.readFile(`data/${queryData.id}`, 'utf8', function(err, data){
+                let description = data;
+                let template = `
+                <!doctype html>
+                <html>
+                <head>
+                  <title>WEB1 - ${title}</title>
+                  <meta charset="utf-8">
+                </head>
+                <body>
+                  <h1><a href="/">WEB</a></h1>
+                  <ol>
+                    <li><a href="/?id=HTML">HTML</a></li>
+                    <li><a href="/?id=CSS"">CSS</a></li>
+                    <li><a href="/?id=JavaScript"">JavaScript</a></li>
+                  </ol>
+                  <h2>${title}</h2>
+                  <p>
+                  ${description}
+                  </p>
+                </body>
+                </html>
+                `;
+                response.writeHead(200);
+                response.end(template);
+            });
+        }
+    } else {
+        response.writeHead(404);
+        response.end('Not found');
     }
-    if (_url === '/favicon.ico') {
-        return response.writeHead(404);
-    }
-    response.writeHead(200);
-    console.log(__dirname + _url);
-    response.end(queryData.id);
 
 });
 app.listen(3000);
---
+++ nodejs/fileread.js
@@ -0,0 +1,4 @@
+let fs = require('fs');
+fs.readFile('sample.txt', 'utf-8', function(err, data){
+    console.log(data);
+});
