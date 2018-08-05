0c0c6fc233e8e79495c0ba39c7aed5c9cc551337
Jingi Kim <copyx00@gmail.com>
2018-08-06 01:47:36 +0900

    WEB2 - Javascript : 라이브러리와 프레임워크
    JQuery를 CDN을 통한 방식으로 사용해볼 수 있었음.

--- js/colors.js
+++ js/colors.js
@@ -1,20 +1,25 @@
 const Links = {
   setColor(color) {
+    /*
     const aList = document.querySelectorAll('a');
     let i = 0;
     while (i < aList.length) {
       aList[i].style.color = color;
       i += 1;
     }
+    */
+    $('a').css('color', color);
   },
 };
 
 const Body = {
   setColor(color) {
-    document.querySelector('body').style.color = color;
+    // document.querySelector('body').style.color = color;
+    $('body').css('color', color);
   },
   setBackgroundColor(color) {
-    document.querySelector('body').style.backgroundColor = color;
+    // document.querySelector('body').style.backgroundColor = color;
+    $('body').css('backgroundColor', color);
   },
 };
 
