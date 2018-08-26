db6fe66d767bba8b2e8ecdf3b80aaca950d28066
Jingi Kim <copyx00@gmail.com>
2018-08-26 22:07:30 +0900

    Javascript FLow : 4-2. closure로 private 변수 만들기
    클로져를 이용해 다른 객체지향 언어에서 사용하는 private 변수처럼 원하는 부분만 외부에 노출시킬 수 있는 코드를 작성하고 배워봄.

--- javascript/closure.js
+++ javascript/closure.js
@@ -9,7 +9,7 @@ let a = function () {
   const x = 1;
   const b = function () {
     console.log(x);
-  }
+  };
   b();
 };
 a();
@@ -21,7 +21,7 @@ a = function () {
   return function () {
     console.log(x);
   };
-}
+};
 let c = a();
 c();
 
@@ -61,3 +61,54 @@ console.log(count());
 console.log(count());
 // "클로져란 이미 생명주기가 끝난 외부함수의 변수를 참조하는 함수"
 // From [Inside Javascript]
+
+// 4-2 Closure로 Private Member 만들기.
+let car = {
+  fuel: 10, // 연료 (l)
+  power: 2, // 연비 (km / l)
+  total: 0,
+  run(km) {
+    const wasteFuel = km / this.power;
+    if (this.fuel < wasteFuel) {
+      console.log('이동불가');
+      return;
+    }
+    this.fuel -= wasteFuel;
+    this.total += km;
+    console.log(`이동거리: ${this.total}`);
+  },
+};
+
+car.run(10);
+car.run(10);
+car.run(10);
+
+// 외부에서 car의 멤버변수들에 접근 가능.
+// 이를 막기위해 private 변수로 변경
+const createCar = function (f, p) {
+  let fuel = f;
+  const power = p;
+  let total = 0;
+  return {
+    run: function (km) {
+      const wasteFuel = km / power;
+      if (fuel < wasteFuel) {
+        console.log('이동불가');
+        return;
+      }
+      fuel -= wasteFuel;
+      total += km;
+      console.log(`이동거리: ${total}`);
+    },
+  };
+};
+car = createCar(10, 2);
+car.run(10);
+car.run(10);
+car.run(10);
+
+// 클로저를 이용해 private 변수를 만드는 방법
+// 1. 함수에서 지역변수 및 내부함수 등을 생성
+// 2. 외부에 노출시키고자 하는 멤버들로 구성된 객체를 return
+// -> return한 객체에 포함되지 않은 멤버들은 private하다.
+// -> return한 객체에 포함된 멤버들은 public하다.
