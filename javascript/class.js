3f8daeb2f90194c33d5631cf4742afd1d19f97ae
Jingi Kim <copyx00@gmail.com>
2018-08-31 02:03:55 +0900

    Javascript FLow : 6-2 class 상속 구현
    자바스크립트에서 클래스 상속처럼 동작하려면 어떻게 해야되고, 내부 동작이 어떻게 되는지 볼 수 있었음.

--- javascript/class.js
+++ javascript/class.js
@@ -30,3 +30,93 @@ console.log(gomu.getAge());
 // console.log(gomu.getInformation(gomu));
 
 console.log(Person.getInformation(gomu));
+
+// 6-2 class 상속 구현
+
+function Man(name, age) {
+  this.name = name || 'NoName';
+  this.age = age || 'NoAge';
+}
+
+Man.prototype.getName = function () {
+  return this.name;
+};
+
+Man.prototype.getAge = function () {
+  return this.age;
+};
+
+function Employee(name, age, position) {
+  // this.name = name || 'NoName';
+  // this.age = age || 'NoAge';
+  this.superClass(name, age);
+  this.position = position || 'NoPosition';
+}
+
+// 메소드가 중복됨
+// Employee.prototype.getName = function() {
+//   return this.name;
+// };
+//
+// Employee.prototype.getAge = function() {
+//   return this.age;
+// };
+
+// 제대로 상속되지 않음. 프로퍼티들이 딸려와서 원하지 않는 값이 나올 수 있음.
+// Employee.prototype = new Man();
+// Employee.prototype.constructor = Employee;
+
+// Bridge 가 매번 만들어 쓰기에는 딱 한번만 사용돼서 의미가 없음.
+// closure 를 이용해 함수를 만들어 두고두고 쓰거라~ by 더글라스 크락포드
+// function Bridge() {}
+// Bridge.prototype = Man.prototype;
+// Employee.prototype = new Bridge();
+// Employee.prototype.constructor = Employee;
+
+const extendClass = (function () {
+  function Bridge() {}
+  return function (parent, child) {
+    Bridge.prototype = parent.prototype;
+    child.prototype = new Bridge();
+    child.prototype.constructor = child;
+    child.prototype.superClass = parent;
+  };
+}());
+extendClass(Man, Employee);
+
+Employee.prototype.getPosition = function () {
+  return this.position;
+};
+
+const gom = new Employee('고무', 30, 'CEO');
+console.dir(gom);
+
+// ES6
+class Human {
+  constructor(name, age) {
+    this.name = name || 'NoName';
+    this.age = age || 'NoAge';
+  }
+
+  getName() {
+    return this.name;
+  }
+
+  getAge() {
+    return this.age;
+  }
+}
+
+class Fighter extends Human {
+  constructor(name, age, position) {
+    super(name, age);
+    this.position = position || 'NoPosition';
+  }
+
+  getPosition() {
+    return this.position;
+  }
+}
+
+const go = new Fighter('Fgo', 29, 'Master');
+console.dir(go);
